17c5f0adb24bff5f2b1c1003bb6037163aca9875
ZiyaMIsk <97843982+ZiyaMIsk@users.noreply.github.com>
2022-03-07 18:00:14 +0300

    Delete RentACar/src/main/core/utilites directory

--- RentACar/src/main/core/utilites/mapping/abstracts/ModelMapperService.java
+++
@@ -1,9 +0,0 @@
-package com.turkcell.RentACar.core.utilites.mapping.abstracts;
-
-import org.modelmapper.ModelMapper;
-
-public interface ModelMapperService {
-
-	ModelMapper forDto();
-	ModelMapper forRequest();
-}
